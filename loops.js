202bba788fe37c42c49ce651e231e77ba311d1f6
Petar Cale <usavatreni@gmail.com>
2020-11-17 03:20:05 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+
+function forLoop(array) {
+  for (let i = 0, i < 25; i++) {
+    if (${i} === 1) {
+   console.log(`I am ${i} strange loop.`)
+    } else {
+      console.log(`I am ${i} strange loops`)
+    }
+  }
+}
